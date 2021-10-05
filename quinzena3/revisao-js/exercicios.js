666599c46bda76038853c811c12fc2b7f1ca013e
Laura Lanna <laura.lannab@gmail.com>
2021-10-05 20:52:04 -0300

    Exercício 6 feito e funcionando

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -54,7 +54,19 @@ function retornaQuantidadeElementos(array) {
 
 // EXERCÍCIO 06
 function retornaExpressoesBooleanas() {
+  const booleano1 = true
+  const booleano2 = false
+  const booleano3 = !booleano2 
+  const booleano4 = !booleano3
 
+  let respostasBooleanos = [booleano1 && booleano2 && !booleano4, 
+    (booleano1 && booleano2) || !booleano3,
+    (booleano2 || booleano3) && (booleano4 || booleano1),
+    !(booleano2 && booleano3) || !(booleano1 && booleano3),
+    !(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)      
+  ]
+
+  return respostasBooleanos
 }
 
 // EXERCÍCIO 07
