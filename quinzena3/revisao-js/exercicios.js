7efe1cff9d8675e2e12538cf372faf3506bf3a89
Laura Lanna <laura.lannab@gmail.com>
2021-10-07 22:54:13 -0300

    Exercício 12 feito e funcionando

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -185,12 +185,18 @@ const novaArray = []
 
 // EXERCÍCIO 12
 function filmeFavorito() {
-
+objeto={
+  nome:"O Diabo Veste Prada",
+  ano: 2006,
+  diretor:"David Frankel",
+  atores:['Meryl Streep', 'Anne Hathaway', 'Emily Blunt', 'Stanley Tucci']
+}
+return objeto
 }
 
 // EXERCÍCIO 13
 function imprimeChamada() {
-  // "Venha assistir ao filme NOME_DO_FILME, de ANO, dirigido por DIRECAO e estrelado por ELENCO."
+
 }
 
 // EXERCÍCIO 14
