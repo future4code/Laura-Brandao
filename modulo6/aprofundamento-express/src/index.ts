d2456652d275aa77161ed2aa20d0b3a9f002a327
Laura Lanna <laura.lannab@gmail.com>
2022-03-31 23:12:55 -0300

    Exercícios aprofundamento express

---
+++ modulo6/aprofundamento-express/src/index.ts
@@ -0,0 +1,46 @@
+import express, { Request, Response } from "express";
+import { toDos } from "./data";
+import {v4 as generateId } from 'uuid'
+
+import { AddressInfo } from "net";
+
+const app = express();
+
+app.use(express.json());
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost:${address.port}`);
+    } else {
+        console.error(`Failure upon starting server.`);
+    }
+});;
+
+app.get("/ping", (req: Request, res: Response) => {
+    res.send("Pong! 🏓")
+});
+
+app.get("/todos", (req: Request, res: Response) => {
+    const taskComplete = toDos.filter((todo) => {
+        if(todo.complete === true){
+            return todo
+        }
+    })  
+    res.send(taskComplete)
+})
+
+app.post("/newTask", (req: Request, res: Response) => {
+    const userName = req.headers.authorization
+    const taskName = req.body.title
+    if(!taskName){
+        res.status(404).send("Título não encontrado")
+        return;
+    }
+    const newTask = {
+        userID: userName,
+        id: generateId(),
+        title: "delectus aut autem",
+        complete: false
+    } 
+})
