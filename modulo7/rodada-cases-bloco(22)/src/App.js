61e1b23ae835e7bdd7aa3f27e39e9f98e4851a39
Laura Lanna <laura.lannab@gmail.com>
2022-07-15 17:17:58 -0300

    Iniciando case - app React+router+chakra

---
+++ modulo7/rodada-cases-bloco(22)/src/App.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <div>oii</div>
+    </ChakraProvider>
+  );
+}
+
+export default App;
