7e435e91feb8685cae5dd0a5354f13f8459526f8
Laura Lanna <laura.lannab@gmail.com>
2022-08-23 21:47:41 -0300

    Case bloco 25

---
+++ modulo9/rodada-cases-bloco(25)/src/model/recipe.ts
@@ -0,0 +1,12 @@
+export type recipe = {
+    id: string;
+    title: string;
+    description: string;
+    creationDate: string;
+}
+
+export interface RecipeinputDTO {
+    title: string;
+    description: string;
+    creationDate: string;
+}
---
+++ modulo9/rodada-cases-bloco(25)/src/model/types.ts
@@ -0,0 +1,3 @@
+export type AuthenticationData = {
+    id: string
+}
